1e811c41072024c967227d51751ea0ba610e9705
YoussefMorsy-Star <youssefmorsi.251@gmail.com>
2025-08-03 10:17:28 +1000

    using random functions and document expected ranges

---
+++ prac_03/randoms.py
@@ -0,0 +1,14 @@
+"""
+Randoms practice
+"""
+
+import random
+
+print(random.randint(5, 20))
+
+print(random.randrange(3, 10, 2))
+
+print(random.uniform(2.5, 5.5))
+
+random_number = random.randint(1, 100)
+print(f"Random number between 1 and 100: {random_number}")
